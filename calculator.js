077c889ac2e145b32ac30a00b0cc526f9ee62766
satishjha-001 <satishjha@engineer.com>
2025-07-21 17:00:24 +0530

    feat: support custom delimiter using //delimiter\\n notation

--- calculator.js
+++ calculator.js
@@ -7,8 +7,17 @@ const add = (numbers) => {
   // if numbers is empty then returning 0
   if (numbers === "") return 0;
 
+  // here support for custom delimiters is added
+  // (e.g., //;\n1;2)
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const match = numbers.match(/^\/\/(.)\n/);
+    delimiter = new RegExp(match[1]);
+    numbers = numbers.slice(match[0].length);
+  }
+
   // it will retun the single number as well sum of comma-seprated numbers and also support new line as delimiters
-  const parts = numbers.split(/,|\n/).map(Number);
+  const parts = numbers.split(delimiter).map(Number);
   return parts.reduce((sum, num) => sum + num, 0);
 };
 
--- calculator.test.js
+++ calculator.test.js
@@ -15,12 +15,17 @@ test('returns sum of two comma-separated numbers', () => {
   expect(add("6,4")).toBe(10);
 });
 
-//  test case for multiple numbers string
+// test case for multiple numbers string
 test('returns sum of multiple numbers', () => {
   expect(add("1,2,3,4")).toBe(10);
 });
 
-//  test case to check newlines between numbers
+// test case to check newlines between numbers
 test('supports newlines between numbers', () => {
   expect(add("1\n2,3")).toBe(6);
+});
+
+// test case to check custom delimiter
+test('supports custom delimiter', () => {
+  expect(add("//;\n1;2")).toBe(3);
 });
